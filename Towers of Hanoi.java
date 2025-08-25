ca1d317b3747f649c916df1dd104d7d872957df6
Robyetul Islam <62092864+RIR-Is-Everywhere@users.noreply.github.com>
2025-08-25 21:05:06 +0600

    Add files via upload

---
+++ Towers of Hanoi.java
@@ -0,0 +1,18 @@
+// Problem: Solve the classic recursion problem with 3 rods.
+
+public class TowersOfHanoi {
+    public static void solve(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + from + " to " + to);
+            return;
+        }
+        solve(n - 1, from, aux, to);
+        System.out.println("Move disk " + n + " from " + from + " to " + to);
+        solve(n - 1, aux, to, from);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        solve(n, 'A', 'C', 'B');
+    }
+}

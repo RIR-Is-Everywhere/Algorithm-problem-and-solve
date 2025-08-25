ea6bb62c59a842e1f0340a5ef3e344c4260dca81
Robyetul Islam <62092864+RIR-Is-Everywhere@users.noreply.github.com>
2025-08-25 22:02:06 +0600

    Add files via upload

---
+++ Matrix Multiplication.java
@@ -0,0 +1,24 @@
+//Problem: Multiply two matrices.
+
+public class MatrixMultiplication {
+    public static void multiply(int[][] A, int[][] B, int n) {
+        int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        for (int[] row : C) {
+            for (int val : row) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] A = {{1, 2}, {3, 4}};
+        int[][] B = {{5, 6}, {7, 8}};
+        multiply(A, B, 2);
+    }
+}
